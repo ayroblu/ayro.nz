0c6a5d2a16f51c3460f533db714c02339bd242b9
Ben Lu <ben@ben-l.com>
2017-02-19 14:59:10 +1300

    Added universal routes package scripts changes

--- server/app.js
+++ server/app.js
@@ -30,44 +30,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Setup logger
 app.use(morgan('combined'))
 
-// Serve static assets
-function universalLoader(req, res) {
-  //res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
-  const filePath = path.resolve(__dirname, '..', 'build', 'index.html')
+const index = require('./routes/index')
+app.use('/', index)
 
-  fs.readFile(filePath, 'utf8', (err, htmlData)=>{
-    if (err) {
-      console.error('err', err)
-      return res.status(404).end()
-    }
-    reactRouter.match({ routes, location: req.url }, (err, redirect, ssrData) => {
-      if(err) {
-        console.error('huh err', err)
-        return res.status(404).end()
-      } else if(redirect) {
-        res.redirect(302, redirect.pathname + redirect.search)
-      } else if(ssrData) {
-        let store = configureStore()
-        const ReactApp = reactDomServer.renderToString(
-          react.createElement(Provider, {store},
-            react.createElement(reactRouter.RouterContext, ssrData)
-          )
-        )
-        const RenderedApp = htmlData.replace('{{SSR}}', ReactApp)
-        res.send(RenderedApp)
-      } else {
-        return res.status(404).end()
-      }
-    })
-  })
-}
-app.use('/', universalLoader)
+// Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
-const api = require('./api')
+const api = require('./routes/api')
 app.use('/api', api)
 
 // Always return the main index.html, so react-router render the route in the client
+const api = require('./api')
 app.use('/', universalLoader)
 
 module.exports = app
